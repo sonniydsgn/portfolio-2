6eeceb8bc4b9e0469b62321740a42e9ea597ef9e
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2024-10-22 22:27:41 +0500

    update package and switch to new eslint config

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js'
+import configPrettier from 'eslint-config-prettier'
+import pluginAstro from 'eslint-plugin-astro'
+import pluginPrettier from 'eslint-plugin-prettier'
+import globals from 'globals'
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+	{
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.es2020,
+				...globals.browser,
+			},
+		},
+	},
+	{
+		plugins: {
+			astro: pluginAstro,
+			prettier: pluginPrettier,
+		},
+	},
+	{
+		ignores: ['node_modules', 'dist'],
+	},
+	js.configs.recommended,
+	...pluginAstro.configs.recommended,
+	{
+		rules: {
+			...configPrettier.rules,
+			'sort-imports': 'off',
+			'import/order': 'off',
+			'no-var': 'error',
+			'prefer-const': 'off',
+			'func-names': 'off',
+		},
+	},
+]
