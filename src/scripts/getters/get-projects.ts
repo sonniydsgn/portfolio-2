4c362e6a8da485d94434d6832dda09d6d15d861d
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2024-11-02 22:18:04 +0500

    update project architecture & start project page & chores

--- src/scripts/getters/get-projects.ts
+++ src/scripts/getters/get-projects.ts
@@ -1,5 +1,17 @@
 import typograph from '@/scripts/libs/typograf'
-import { getCollection } from 'astro:content'
+import { type CollectionEntry, getCollection } from 'astro:content'
+
+const allVideos = import.meta.glob('/src/content/projects/media/**/**/*.{mp4,webm}', {
+	eager: true,
+})
+
+const getVideo = (format: 'mp4' | 'webm', item: CollectionEntry<'projects'>) => {
+	return Object.entries(allVideos)
+		.filter((video: any) => {
+			return video[0].includes(item.data.cover[format]?.slice(1))
+		})
+		.map((video: any) => video[1].default) as unknown as string
+}
 
 const projects = async () => {
 	return (await getCollection('projects')).map(item => {
@@ -9,6 +21,11 @@ const projects = async () => {
 				...item.data,
 				name: typograph.execute(item.data.name),
 				desc: typograph.execute(item.data.desc),
+				cover: {
+					...item.data.cover,
+					mp4: getVideo('mp4', item),
+					webm: getVideo('webm', item),
+				},
 			},
 		}
 	})
