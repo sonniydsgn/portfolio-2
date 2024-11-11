3ca79e284424494457780f81556839addadcbedd
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2024-11-11 14:37:14 +0500

    improve project pages generating

--- src/content/config.ts
+++ src/content/config.ts
@@ -24,6 +24,35 @@ const projectsCollection = defineCollection({
 				logo: image(),
 				link: z.string(),
 			}),
+			roles: z.array(
+				z.object({
+					name: z.string(),
+					desc: z.string(),
+					icon: z.string(),
+				}),
+			),
+			team: z
+				.array(
+					z.object({
+						name: z.string(),
+						desc: z.string(),
+						image: image().optional(),
+					}),
+				)
+				.optional(),
+			timeline: z.object({
+				period: z.string(),
+				status: z.string(),
+			}),
+			highlights: z
+				.array(
+					z.object({
+						src: image(),
+						alt: z.string(),
+						title: z.string().optional(),
+					}),
+				)
+				.optional(),
 			result: z.string().optional(),
 			draft: z.boolean(),
 		}),
