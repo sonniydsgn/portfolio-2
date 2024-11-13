31e992a42fc961f826b43dbf2482a2e410f1c354
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2024-11-14 00:28:01 +0500

    take out cover components & add covers for videos

--- src/scripts/getters/get-highlight.ts
+++ src/scripts/getters/get-highlight.ts
@@ -7,7 +7,9 @@ const getHighlightSource = (type: 'video' | 'image', source: string | string[]) 
 		.filter(highlight => {
 			return type === 'image'
 				? highlight[0].includes((source as string).slice(1))
-				: highlight[0].includes(source[0].slice(1)) || highlight[0].includes(source[1].slice(1))
+				: highlight[0].includes(source[0].slice(1)) ||
+						highlight[0].includes(source[1].slice(1)) ||
+						highlight[0].includes(source[2].slice(1))
 		})
 		.map(item => (Object.values(item)[1] as any).default)
 }
