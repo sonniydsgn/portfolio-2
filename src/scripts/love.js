6c3220096479d3c4cdcc3624916349f6c60b5324
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2025-02-08 21:15:06 +0500

    update social links

--- src/scripts/love.js
+++ src/scripts/love.js
@@ -2,10 +2,10 @@ function love() {
 	setTimeout(
 		console.log.bind(
 			console,
-			`%cс ❤️ сонный дизайнер  |  https://sonniydesign.ru%c`,
-			"background: #000;color:#FFF;padding:8px 16px;border-radius: 8px;",
-			""
-		)
+			`%cс ❤️ Алексей КАлинин  |  https://designbykalinin.ru%c`,
+			'background: #000;color:#FFF;padding:8px 16px;border-radius: 8px;',
+			'',
+		),
 	)
 }
 love()
