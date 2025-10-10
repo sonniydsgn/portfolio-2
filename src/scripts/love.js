d5215dc4a9dbcb6964a539c1fe44ab6521aca3f5
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2025-10-11 01:26:33 +0500

    feat: lazy load project card covers

--- src/scripts/love.js
+++ src/scripts/love.js
@@ -2,7 +2,7 @@ function love() {
 	setTimeout(
 		console.log.bind(
 			console,
-			`%cс ❤️ Алексей КАлинин  |  https://designbykalinin.ru%c`,
+			`%cс ❤️ Алексей Калинин  |  https://designbykalinin.ru%c`,
 			'background: #000;color:#FFF;padding:8px 16px;border-radius: 8px;',
 			'',
 		),
